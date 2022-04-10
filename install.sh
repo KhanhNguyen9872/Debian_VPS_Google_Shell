beefb7625aeb0c2d204dab14798337c47171be70
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-04-10 20:53:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "Installing Linux (Debian amd64)...."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y > /dev/null 2>&1
-sudo apt install libc-bin base-files sysvinit-utils tzdata bazel apt-transport-https ufw fish apache2 php sublime-text xfce4 xarchiver firefox-esr mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openjdk-17-jdk openssl libreoffice hardinfo docker-compose tigervnc-standalone-server tigervnc-xorg-extension docker -y
+sudo apt install libc-bin base-files sysvinit-utils tzdata bazel apt-transport-https ufw fish apache2 php sublime-text xfce4 xarchiver wine firefox-esr mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openjdk-17-jdk openssl libreoffice hardinfo docker-compose tigervnc-standalone-server tigervnc-xorg-extension docker -y
 export HOME="$(pwd)"
 export DISPLAY=":0"
 cd $HOME 2> /dev/null
@@ -34,9 +34,12 @@ wget -O vscode.deb "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/b
 sudo mv ./startvps.sh /bin/startvps 2> /dev/null
 sudo rm -rf ~/.bashrc 2> /dev/null
 sudo mv ./setupPS.sh ~/.bashrc 2> /dev/null
+sudo rm -f /bin/wine 2> /dev/null
+sudo ln -s /etc/alternatives/wine64 /bin/wine 2> /dev/null
 sudo chmod 777 -R ~/.vnc 2> /dev/null
 sudo chmod 777 ~/.bashrc 2> /dev/null
 sudo chmod 777 /bin/startvps 2> /dev/null
+sudo chmod 777 /bin/wine 2> /dev/null
 dpkg -i vscode.deb
 rm -rf ./vscode.deb 2> /dev/null
 sudo apt update -y > /dev/null 2>&1
