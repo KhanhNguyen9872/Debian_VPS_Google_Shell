5228743f04e4b723eab84782f6ebcf1414c0a912
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-04-10 17:33:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,14 +12,16 @@ ngrok authtoken $key
 echo ""
 echo "Installing Linux (Debian amd64)...."
 sudo apt update -y > /dev/null 2>&1
-sudo apt install xfce4 xarchiver firefox-esr mesa-utils xfce4-goodies apt-utils dialog dbus-x11 dbus perl p7zip unzip zip curl tar git python3 python3-pip net-tools tigervnc-standalone-server tigervnc-xorg-extension -y
+sudo apt install xfce4 xarchiver firefox-esr mesa-utils git xfce4-goodies pv nano apt-utils dialog dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools tigervnc-standalone-server tigervnc-xorg-extension -y
 export HOME="/root"
 export DISPLAY=":0"
 cd $HOME 2> /dev/null
 sudo mkdir .vnc 2> /dev/null
 sudo printf '#!/bin/bash\ndbus-launch &> /dev/null\nxfce4-session\n' > .vnc/xstartup
 wget -O startvps.sh "https://raw.githubusercontent.com/KhanhNguyen9872/Ubuntu_VPS_Google_Shell/main/startvps.sh" 2> /dev/null
-sudo mv ./startvps.sh /bin/startvps
+wget -O setupPS.sh "https://raw.githubusercontent.com/KhanhNguyen9872/Ubuntu_VPS_Google_Shell/main/setupPS.sh" 2> /dev/null
+sudo mv ./startvps.sh /bin/startvps 2> /dev/null
+sudo mv ./setupPS.sh ~/.bash_profile 2> /dev/null
 sudo chmod 777 .vnc/xstartup 2> /dev/null
 sudo chmod 777 /bin/startvps 2> /dev/null
 sudo startvps
