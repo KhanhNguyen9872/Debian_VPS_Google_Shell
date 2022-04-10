9c0b2a6ba9864c0e540727934787c64cc5368a30
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-04-10 17:12:24 +0700

    Update startvps.sh

--- startvps.sh
+++ startvps.sh
@@ -3,8 +3,9 @@ unset DBUS_LAUNCH
 export HOME="/root"
 export DISPLAY=":0"
 nohup ngrok tcp -region=ap 5900 &>/dev/null &
-nohup vncserver :0 &> /dev/null &
+vncserver :0
 clear
 printf "\nYour IP Here: "
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 printf "\n\n"
