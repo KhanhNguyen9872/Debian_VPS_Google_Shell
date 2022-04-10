f1af2c9e7d20a24b32917d2eb8cac916c6f6a152
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-04-10 20:24:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "Installing Linux (Debian amd64)...."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y > /dev/null 2>&1
-sudo apt install libc-bin base-files sysvinit-utils tzdata bazel apt-transport-https ufw sublime-text xfce4 xarchiver firefox-esr mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openjdk-17-jdk openssl libreoffice hardinfo docker-compose tigervnc-standalone-server tigervnc-xorg-extension docker -y
+sudo apt install libc-bin base-files sysvinit-utils tzdata bazel apt-transport-https ufw fish sublime-text xfce4 xarchiver firefox-esr mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openjdk-17-jdk openssl libreoffice hardinfo docker-compose tigervnc-standalone-server tigervnc-xorg-extension docker -y
 export HOME="$(pwd)"
 export DISPLAY=":0"
 cd $HOME 2> /dev/null
